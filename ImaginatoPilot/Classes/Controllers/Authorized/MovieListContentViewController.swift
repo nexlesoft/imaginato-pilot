51c9e3904d3da27bb5c0e48fdef931a3b31205fa
NgocLK <ngoclk@nexlesoft.com>
2018-12-20 16:26:55 +0700

    Create page view for movie list

---
+++ ImaginatoPilot/Classes/Controllers/Authorized/MovieListContentViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MovieListContentViewController.swift
+//  ImaginatoPilot
+//
+//  Created by admin on 12/20/18.
+//  Copyright © 2018 GPThanh. All rights reserved.
+//
+
+import UIKit
+
+class MovieListContentViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
