9592f4ff2d67558218c011af17137dc1a2a2c1ab
NgocLK <ngoclk@nexlesoft.com>
2018-12-20 22:51:29 +0700

    small fix movieCell

--- ImaginatoPilot/Classes/Controllers/Authorized/MovieVC/MovieListContentViewController.swift
+++ ImaginatoPilot/Classes/Controllers/Authorized/MovieVC/MovieListContentViewController.swift
@@ -17,11 +17,7 @@ class MovieListContentViewController: UIViewController {
     var keyword = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        if pageIndex == 0 {
-            
-        } else {
-            
-        }
+        
 //        let obsKeyword = Observable<String>.just("s")
 //        obsKeyword.bind(to: viewModel.searchText)
 //            .disposed(by: disposeBag)
@@ -50,14 +46,18 @@ class MovieListContentViewController: UIViewController {
         obsKeyword.bind(to: viewModel.searchText)
             .disposed(by: disposeBag)
 
-        viewModel.showingData
-            .drive(tableView.rx.items(cellIdentifier: "MovieListCell")) { _, movie, cell in
-                if let movieCell = cell as? MovieListCell {
-                    movieCell.titleLabel.text = movie.title
+        if pageIndex == 0 {
+            viewModel.showingData
+                .drive(tableView.rx.items(cellIdentifier: "MovieListCell")) { _, movie, cell in
+                    if let movieCell = cell as? MovieListCell {
+                        movieCell.titleLabel.text = movie.title
+                        movieCell.posterImage.layer.cornerRadius = 4
+//                        movieCell.posterImage.sd_setImage(with: URL(string: movie.pos), completed: nil)
+                    }
                 }
-                //                cell.textLabel?.text = movie.title
-                //                cell.detailTextLabel?.text = movie.id
-            }
-            .disposed(by: disposeBag)
+                .disposed(by: disposeBag)
+        } else {
+            
+        }
     }
 }
