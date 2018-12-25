8c55d8debbfa8c9ab029f3a6ce1d5b3e530fe186
Trai Nguyen <trainv@nexlesoft.com>
2018-12-25 18:10:52 +0700

    Update mvvm structure

--- ImaginatoPilot/Classes/ViewModels/HomeViewModel.swift
+++ ImaginatoPilot/Classes/ViewModels/HomeViewModel.swift
@@ -12,8 +12,28 @@ import RxCocoa
 import SwiftyJSON
 
 class HomeViewModel {
-    var arrMovie = Variable<[MovieDTO]>([])
+    var arrMovie = Variable<[MovieViewModel]>([])
     var completionFetchData: (()->())?
+    
+    let disposeBag = DisposeBag()
+    
+    var centeredTitle: BehaviorSubject<String> = BehaviorSubject<String>(value: "")
+    var centeredType: BehaviorSubject<String> = BehaviorSubject<String>(value: "")
+    var centeredIndex: BehaviorSubject<Int> = BehaviorSubject<Int>(value: -1) {
+        didSet {
+            do {
+                let value = try centeredIndex.value() as Int
+                if (value > -1) && (value < self.arrMovie.value.count) {
+                    let movie = arrMovie.value[value]
+                    centeredTitle.onNext(movie.displayTitle)
+                    let type: String = movie.genreIds.map{$0.name ?? ""}.joined(separator: ", ")
+                    centeredType.onNext(type)
+                }
+            } catch  {
+                print("Invalid Index.")
+            }
+        }
+    }
     
     let baseWebServices: BaseWebServices
     init(baseWebServices: BaseWebServices) {
@@ -22,7 +42,11 @@ class HomeViewModel {
     
     func fetchMovieList() {
         baseWebServices.getMovieList(path: "home", success: { (arrMovie) in
-            self.arrMovie.value = arrMovie
+            arrMovie.forEach({ (movie) in
+                let vm = MovieViewModel()
+                vm.movie = movie
+                self.arrMovie.value.append(vm)
+            })
             if let completion = self.completionFetchData {
                 completion()
             }
